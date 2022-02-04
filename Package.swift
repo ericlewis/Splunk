c9934683f6dfa21acb811952926ac30d018e65f5
Eric Lewis <ericlewis777@gmail.com>
2022-02-04 02:21:07 -0500

    breaking: bump min versions to 14 etc

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "Splunk",
     platforms: [
-      .iOS("13.0"),
-      .macOS("10.15"),
-      .tvOS("13.0"),
-      .watchOS("6.0")
+      .iOS("14.0"),
+      .macOS("11.0"),
+      .tvOS("14.0"),
+      .watchOS("7.0")
     ],
     products: [
         .library(name: "Splunk",
