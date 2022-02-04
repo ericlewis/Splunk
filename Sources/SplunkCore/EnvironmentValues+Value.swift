7de932066cb225551164653eb31c87fee4bcb448
Eric Lewis <ericlewis777@gmail.com>
2022-02-04 02:21:31 -0500

    fix: handle iOS 14 correctly.

--- Sources/SplunkCore/EnvironmentValues+Value.swift
+++ Sources/SplunkCore/EnvironmentValues+Value.swift
@@ -1,10 +1,24 @@
 import SwiftUI
+import OSLog
 
 extension EnvironmentValues {
+  func findEnvironment() -> Any? {
+    if #available(iOS 15, watchOS 8, tvOS 15, macOS 12, *) {
+      return Mirror(reflecting: self).descendant("_plist", "elements", "some")
+    } else {
+      return Mirror(reflecting: self).descendant("plist", "elements", "some")
+    }
+  }
   /// Find arbitrary environment values with a key & type.
   public func value<T>(of key: String, as: T.Type = T.self) -> T? {
+    let logger = Logger(subsystem: "com.splunk", category: key)
     let desiredKey = "TypedElement<EnvironmentPropertyKey<\(key)>>"
-    guard let environment = Mirror(reflecting: self).descendant("_plist", "elements", "some") else { return nil }
+    guard let environment = findEnvironment() else {
+      #if DEBUG
+      logger.error("could not find environment.")
+      #endif
+      return nil
+    }
     func visit(_ node: Any) -> T? {
       let currentKey = String(describing: type(of: node))
       let mirror = Mirror(reflecting: node)
