5b93f0e15536ba6b41bd7fea9eecc9d0aa49b4a6
Eric Lewis <ericlewis777@gmail.com>
2022-02-16 22:20:18 -0500

    feat: unsafe value

--- Sources/SplunkCore/EnvironmentValues+Value.swift
+++ Sources/SplunkCore/EnvironmentValues+Value.swift
@@ -41,4 +41,34 @@ extension EnvironmentValues {
     }
     return visit(environment)
   }
+
+  public func _value(of key: String) -> Any? {
+    #if DEBUG
+    let logger = Logger(subsystem: "com.splunk", category: key)
+    #endif
+
+    let desiredKey = "TypedElement<EnvironmentPropertyKey<\(key)>>"
+    guard let environment = Mirror(reflecting: self).descendant(0, 0, "some") else {
+      #if DEBUG
+      logger.error("could not find environment.")
+      #endif
+      return nil
+    }
+    func visit(_ node: Any) -> Any? {
+      let currentKey = String(describing: type(of: node))
+      let mirror = Mirror(reflecting: node)
+      if currentKey == desiredKey {
+        return mirror.descendant(0)
+      }
+
+      guard let nextNode = mirror.superclassMirror?.descendant("after", "some") else {
+        #if DEBUG
+        logger.error("could not find key: \(key).")
+        #endif
+        return nil
+      }
+      return visit(nextNode)
+    }
+    return visit(environment)
+  }
 }
